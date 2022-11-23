fc6ed927d32ff0bfd39eef4f46093878e1a8d922
Rabbe <onikmahmudrabbe@gmail.com>
2022-11-24 02:06:59 +0600

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
@@ -18,10 +19,29 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    // console.log('token inside VerifyJWT', req.headers.authorization);
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access')
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
+
 async function run() {
     try {
         const appointmentOptionCollection = client.db('doctorsPortal').collection("appointmentOptions");
         const bookingsCollection = client.db('doctorsPortal').collection('bookings');
+        const usersCollection = client.db('doctorsPortal').collection('users');
 
         // Use Aggrigate to query multiple collection and then merge data
         app.get('/appointmentOptions', async (req, res) => {
@@ -93,10 +113,24 @@ async function run() {
           * API Naming Convention 
           * app.get('/bookings')
           * app.get('/bookings/:id')
-          * app.post('/bookings')
+          * app.post('/bookings')    (Create post)
           * app.patch('/bookings/:id')
           * app.delete('/bookings/:id')
          */
+
+        app.get('/bookings', verifyJWT, async (req, res) => {
+            const email = req.query.email;
+            // console.log(email);
+            const decodedEmail = req.decoded.email;
+
+            if (email !== decodedEmail) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            const query = { email: email };
+
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
 
         app.post('/bookings', async (req, res) => {
             const booking = req.body
@@ -115,7 +149,47 @@ async function run() {
 
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
+        });
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token });
+            }
+            // console.log(user);
+            res.status(403).send({ accessToken: '' })
         })
+
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.put('/users/admin/:id', verifyJWT, async (req, res) => {
+            const decodedEmail = req.params.email;
+            const query = { email: decodedEmail };
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+
     }
     finally {
 
